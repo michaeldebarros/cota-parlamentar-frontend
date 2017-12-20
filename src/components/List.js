6c80484bb7645b79edc6d73db59601f0dd475378
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2017-12-20 10:47:41 -0200

    added numeroRestituicao as kkey

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const List = props => {
   let arrayResults = props.results.map(result => (
-    <li key={Math.random()}>
+    <li key={result.numeroRestituicao}>
     Nome: {result.nome}; valor: R$ {result.valor},00; data: {result.data}; fonecedor: {result.fornecedor}.
     </li>
   ));
