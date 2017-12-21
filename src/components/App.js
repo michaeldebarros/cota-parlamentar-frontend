d95a29df4a5770011f220cdefc3a4831b31afa18
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2017-12-21 14:26:38 -0200

    better list styles and better management of empty requests

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import '../App.css';
 import Form from './Form';
 
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Cota Parlamentar</h1>
-        </header>
-        <div>
-          <Form />
-        </div>
-      </div>
-
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Cota Parlamentar API</h1>
+				</header>
+				<div className="App-body">
+					<div>
+						<Form />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const List = props => {
-  let arrayResults = props.results.map(result => (
-    <li key={result.numeroRestituicao}>
-    Nome: {result.nome}; valor: R$ {result.valor},00; data: {result.data}; fonecedor: {result.fornecedor}.
-    </li>
-  ));
-  return <ul>{arrayResults}</ul>;
+	console.log(props);
+	if (props.results === 'Não há dados para essa requisição.') {
+		return <p> Não há dados para essa requisição</p>;
+	}
+	let arrayResults = props.results.map(result => (
+		<li key={result.numeroRestituicao}>
+			Nome: {result.nome}; valor: R$ {result.valor},00; data: {result.data}; fonecedor: {result.fornecedor}.
+		</li>
+	));
+	return <ul>{arrayResults}</ul>;
 };
 
 export default List;
