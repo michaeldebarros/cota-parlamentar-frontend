090eba74be11f5b374ffc4a150cdd87c40b28039
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2018-02-17 16:12:48 -0200

    api rout

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,7 +36,7 @@ class Form extends React.Component {
 	handleSubmit(e) {
 		e.preventDefault();
 		this.setState({ results: [], loading: true });
-		const url = `/${this.state.ano}/${this.state.nome}/${this.state.despesa}`;
+		const url = `/api/${this.state.ano}/${this.state.nome}/${this.state.despesa}`;
 		axios
 			.get(url)
 			.then(response => {
@@ -69,8 +69,8 @@ class Form extends React.Component {
 							<option value="AFONSO MOTTA">AFONSO MOTTA PDT-RS</option>
 							<option value="AGUINALDO RIBEIRO">AGUINALDO RIBEIRO PP-PB</option>
 							<option value="ALAN RICK">ALAN RICK DEM-AC</option>
-							<option value="ALBERTO FILHO">ALBERTO FILHO PMDB-MA</option>
 							<option value="ALBERTO FRAGA">ALBERTO FRAGA DEM-DF</option>
+							<option value="ALBERTO FILHO">ALBERTO FILHO PMDB-MA</option>
 							<option value="ALCEU MOREIRA">ALCEU MOREIRA PMDB-RS</option>
 							<option value="ALESSANDRO MOLON">ALESSANDRO MOLON REDE-RJ</option>
 							<option value="ALEX CANZIANI">ALEX CANZIANI PTB-PR</option>
