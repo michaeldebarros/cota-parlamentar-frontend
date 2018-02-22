0ce8edd9920fd8a15ec89cdc46de208852dc22a4
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2018-02-22 08:18:56 -0300

    added 6 seconds timeout ping

--- src/components/Form.js
+++ src/components/Form.js
@@ -41,10 +41,17 @@ class Form extends React.Component {
 			.get(url)
 			.then(response => {
 				this.setState({ results: response.data, loading: false });
+				console.log(response.data.leng);
 			})
 			.catch(error => {
-				this.setState({ results: "server timeout", loading: false });
-				console.log(error);
+				if (error.response.status === 503) {
+					setTimeout(() => {
+						axios.get(`/api/second`)
+							.then(response => {
+								this.setState({ results: response.data, loading: false });
+							})
+					}, 6000);
+				}
 			});
 	}
 
--- src/components/List.js
+++ src/components/List.js
@@ -6,12 +6,10 @@ const List = props => {
 	if (props.loading) {
 		return <RotateSpinLoader />
 	}
-	if (props.results === 'Não há dados para essa requisição.') {
+	if (props.results === "No Register") {
 		return <p> Não há dados para essa requisição</p>;
 	}
-	else if (props.results === "server timeout") {
-		return <p>Sua requisição está demorando mais do que o esperado, por favor tente novamente. Lamentamos o inconveniente.</p>
-	}
+
 	let arrayResults = props.results.map(result => (
 		<li key={result.numeroRestituicao}>
 			Nome: {result.nome}; valor: R$ {result.valor},00; data: {result.data}; fonecedor: {result.fornecedor}.
